83edc95b9b308a97e94ed9ac454f4d88e831c687
Jin Ming Wu <JM@MacBook-Pro-de-Jin.local>
2024-08-19 20:32:22 -0400

    Modulo de division

---
+++ dividedanger.py
@@ -0,0 +1,12 @@
+def dividedanger():
+    print('Please enter two numbers to divide.')
+    dividend = int(input('Please enter the dividend: '))
+    divisor = int(input('Please enter the divisor: '))
+    if divisor != 0:
+        return(dividend, '/', divisor, '=', dividend / divisor)
+    else:
+        return("Division by zero is not allowed.")
+
+
+x = dividedanger()
+print(x)
